6443d6825d8aa3ee40edcdf876c87a0011804f1c
Victor Lomuller <victor@codeplay.com>
2025-02-27 21:56:10 +0000

    fix merge

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -7112,8 +7112,10 @@ ExprResult Sema::BuildResolvedCallExpr(Expr *Fn, NamedDecl *NDecl,
   }
 
   // Diagnose function pointers in SYCL.
-  if (!FDecl && !getLangOpts().SYCLAllowFuncPtr && getLangOpts().SYCLIsDevice &&
-      !isUnevaluatedContext()) {
+  if (!FDecl &&
+      getLangOpts().getSYCLAllowFuncPtr() ==
+          LangOptions::SYCLFuncPtrPreference::Off &&
+      getLangOpts().SYCLIsDevice && !isUnevaluatedContext()) {
     bool MaybeConstantExpr = false;
     Expr *NonDirectCallee = TheCall->getCallee();
     if (!NonDirectCallee->isValueDependent())
